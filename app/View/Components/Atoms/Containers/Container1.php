f6100b9b7c2fbfacab04bef17d59dd18353c0918
dreamersvalera <valera@dreamersofday.co>
2024-07-20 07:13:55 +0300

    feat: create Container1

---
+++ app/View/Components/Atoms/Containers/Container1.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components\Atoms\Containers;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Container1 extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.atoms.containers.container1');
+    }
+}
---
+++ resources/views/components/atoms/containers/container1.blade.php
@@ -0,0 +1,3 @@
+<div data-component='{{ $componentName }}' class="container mx-auto px-4">
+    {{ $slot }}
+</div>
